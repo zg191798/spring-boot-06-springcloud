aa96574ce03299ae06b7ae37baac83c16b5f9075
zeke_glb <zg_zeke@126.com>
2019-01-21 16:05:17 +0800

    Initial commit

---
+++ consumer-user/src/main/java/com/atguigu/consumeruser/ConsumerUserApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.consumeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerUserApplication.class, args);
+    }
+
+}
+
